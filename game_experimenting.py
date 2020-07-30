1b33ebc21a390df19eb502561a20629092ce3b77
Pontus Larsson <pontus.larsson.torner@gmail.com>
2020-07-30 10:09:36 +0200

    my code so far

---
+++ game_experimenting.py
@@ -0,0 +1,40 @@
+# Removing error line under import by using this useless comment
+import pygame
+import sys
+
+from pygame.locals import *
+
+# For using Pygame library and must be added before any other pygame function,
+# else an initialization error may occur.
+
+pygame.init()
+
+""" Game loop begins. Every game that exists has what is called a Game Loop.
+It's merely a concept, not some kind of special syntax or function to be called.
+
+The Game Loop is where all the game events occur, update and get drawn to the screen.
+
+Once the initial setup and initialization of variables is out of the way,
+the Game Loop begins where the program keeps looping over and over until an event
+of type QUIT occurs. """
+
+# Game Loop begins:
+
+while True:
+    pygame.display.update()
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+
+
+# Changes in the game are not implemented until the display.update() has been called.
+# Since games are constantly changing values, the update function is in the game loop,
+# constantly updating.
+
+# EVENT OBJECTS: An "event" occurs when the user performs a specific action, such as
+# clicking the mouse or pressing a keyboard button. Pygame records each and every
+# event that occurs. Find out which events have happened by calling the
+# pygame.event.get() function.
+# One of the many attributes (or properties) held by EVENT OBJECTS is TYPE.
+# The TYPE attribute tells us what kind of EVENT the OBJECT represents.
