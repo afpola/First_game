ef48d5166b73cf799cc7e994aab1e37d357e31a7
Pontus Larsson <pontus.larsson.torner@gmail.com>
2020-08-04 13:25:52 +0200

    added load_image function (fixes #2)

--- mygame.py
+++ mygame.py
@@ -24,8 +24,8 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
 # Other Variables for use in the program
-SCREEN_WIDTH = 484
-SCREEN_HEIGHT = 640
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 484
 SPEED = 5
 SCORE = 0
 
@@ -38,15 +38,88 @@ GAME_OVER = FONT.render("Game Over", True, BLACK)
 """ TASK Create background & import
 This path function is used for saving time and space, not having to type out or paste path
 """
-ASSETS_PATH = 'C:/Users/46707/Work/firstgame/Import_files'
 
-BACKGROUND = pygame.image.load("{}/pitch640x484.png".format(ASSETS_PATH))
 
+def assets_folder(filename):
+    """Returns absolute path to the assets folder
+
+    Args:
+        filename (string): the filename
+
+    Returns:
+        string: the absolute path
+    """
+    assets_path = 'C:/Users/46707/Work/firstgame/Import_files'
+    return "{}/{}".format(assets_path, filename)
+
+
+def load_image(filename):
+    """loads image from the assets folder
+
+    Args:
+        filename (string): the filename
+
+    Returns:
+        string: the image object
+    """
+    return pygame.image.load(assets_folder(filename))
+
+
+BACKGROUND = load_image("pitch640x484.png")
 
 # Create a white screen
-DISPLAYSURF = pygame.display.set_mode((640, 484))
+DISPLAYSURF = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 DISPLAYSURF.fill(WHITE)
 pygame.display.set_caption("Game")
+
+"""
+Creating a class for my player
+
+
+- Future possible Task: Maybe be able to move up and down as well. Don't know.
+
+"""
+
+
+class Player(pygame.sprite.Sprite):
+    """Creating a class for my player
+
+    Args:
+        pygame ([type]): [description]
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.image = load_image("Player.png")
+        self.surf = pygame.Surface((99, 126))
+        self.rect = self.surf.get_rect(
+            center=(SCREEN_WIDTH / 2, SCREEN_HEIGHT * 0.8))
+
+    def update(self):
+        """sssss
+        """
+        pressed_keys = pygame.key.get_pressed()
+       # if pressed_keys[K_UP]:
+        # self.rect.move_ip(0, -5)
+       # if pressed_keys[K_DOWN]:
+        # self.rect.move_ip(0,5)
+
+        if self.rect.left > 0 and pressed_keys[K_LEFT]:
+            self.rect.move_ip(-SPEED, 0)
+        if self.rect.right < SCREEN_WIDTH and pressed_keys[K_RIGHT]:
+            self.rect.move_ip(SPEED, 0)
+
+
+# Setting up Sprites
+P1 = Player()
+# E1 = Enemy()
+
+# Creating Sprites Groups
+# enemies = pygame.sprite.Group()
+# enemies.add(E1)
+ALL_SPRITES = pygame.sprite.Group()
+ALL_SPRITES.add(P1)
+# all_sprites.add(E1)
 
 
 # Beginning Game Loop
@@ -58,3 +131,8 @@ while True:
             sys.exit()
 
     DISPLAYSURF.blit(BACKGROUND, (0, 0))
+
+# Moves and Re-draws all Sprites
+    for entity in ALL_SPRITES:
+        DISPLAYSURF.blit(entity.image, entity.rect)
+        entity.update()
