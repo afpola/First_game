75f1cc7f4efe93f73d75dee61febfca1e9bed892
Pontus Larsson <pontus.larsson.torner@gmail.com>
2020-08-18 16:04:43 +0200

    set up collision detection test

--- mygame.py
+++ mygame.py
@@ -5,6 +5,7 @@ There are a couple of additional imports (random, time) that are used in Game.py
 """
 import sys
 import pygame
+from pygame import sprite, display, time, font, event
 from pygame.locals import *  # pylint: disable=wildcard-import, unused-wildcard-import
 from gamemodels import Player, Goalkeeper, Ball
 from utility import load_image
@@ -12,19 +13,19 @@ from constants import SCREEN_HEIGHT, SCREEN_WIDTH, WHITE, BLACK
 
 pygame.init()
 
-FRAME_PER_SEC = pygame.time.Clock()
+FRAME_PER_SEC = time.Clock()
 
 # Setting up Fonts
-FONT = pygame.font.SysFont("Verdana", 60)
-FONT_SMALL = pygame.font.SysFont("Verdana", 20)
+FONT = font.SysFont("Verdana", 60)
+FONT_SMALL = font.SysFont("Verdana", 20)
 GAME_OVER = FONT.render("Game Over", True, BLACK)
 
 BACKGROUND = load_image("pitch640x484.png")
 
 # Create a white screen
-DISPLAYSURF = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+DISPLAYSURF = display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 DISPLAYSURF.fill(WHITE)
-pygame.display.set_caption("Game")
+display.set_caption("Game")
 
 # Setting up Sprites
 P1 = Player()
@@ -34,18 +35,23 @@ B1 = Ball()
 # Creating Sprites Groups
 # enemies = pygame.sprite.Group()
 # enemies.add(E1)
-ALL_SPRITES = pygame.sprite.Group()
+ALL_SPRITES = sprite.Group()
 ALL_SPRITES.add(P1, E1, B1)
+BALL_GROUP = sprite.Group()
+BALL_GROUP.add(B1)
 # all_sprites.add(E1)
 
 
 # Beginning Game Loop
 while True:
-    pygame.display.update()
-    for event in pygame.event.get():
-        if event.type == QUIT:
+    display.update()
+    for ev in event.get():
+        if ev.type == QUIT:
             pygame.quit()
             sys.exit()
+
+    if sprite.spritecollide(P1, BALL_GROUP, False):
+        print("Test successful")
 
     DISPLAYSURF.blit(BACKGROUND, (0, 0))
 
