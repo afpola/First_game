e702e6e8e3a5cf8c33ba3ad44ad62bfeeaec471b
Pontus Larsson <pontus.larsson.torner@gmail.com>
2020-08-29 23:55:29 +0200

    various changes

--- gamemodels.py
+++ gamemodels.py
@@ -2,7 +2,7 @@
 """
 import pygame
 from pygame.locals import *  # pylint: disable=wildcard-import, unused-wildcard-import
-from utility import load_image
+from utility import load_image, position_from_center
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT, SPEED, GOALKEEPER_SPEED  # , RED
 #from mygame import DISPLAYSURF
 # pylint: disable=pointless-string-statement
@@ -25,28 +25,33 @@ class Player(pygame.sprite.Sprite):
         super().__init__()
         self.image = load_image("Player.png")
         self.surf = pygame.Surface((99, 126))
-        self.rect = self.surf.get_rect(
-            center=(SCREEN_WIDTH / 2, SCREEN_HEIGHT * 0.8))
+        self.rect = self.surf.get_rect()
+        self.reset()
+
+    def reset(self):
+        """fix descript later
+        """
+        position_from_center(self.rect, 0, 150)
 
     def update(self):
-        """sssss
+        """fix descript later
         """
-        movement = pygame.math.Vector2()
+        player_movement = pygame.math.Vector2()
 
         pressed_keys = pygame.key.get_pressed()
 
         if self.rect.left > 0 and pressed_keys[K_LEFT]:
-            movement.x = movement.x - SPEED
+            player_movement.x = player_movement.x - SPEED
         if self.rect.right < SCREEN_WIDTH and pressed_keys[K_RIGHT]:
-            movement.x = movement.x + SPEED
+            player_movement.x = player_movement.x + SPEED
         if self.rect.top > 0 and pressed_keys[K_UP]:
-            movement.y = movement.y - SPEED
+            player_movement.y = player_movement.y - SPEED
         if self.rect.bottom < SCREEN_HEIGHT and pressed_keys[K_DOWN]:
-            movement.y = movement.y + SPEED
+            player_movement.y = player_movement.y + SPEED
 
-        if movement.length() > 0:
-            movement = movement.normalize() * SPEED
-            self.rect.move_ip(movement.x, movement.y)
+        if player_movement.length() > 0:
+            player_movement = player_movement.normalize() * SPEED
+            self.rect.move_ip(player_movement.x, player_movement.y)
 
 
 class Goalkeeper(pygame.sprite.Sprite):
--- utility.py
+++ utility.py
@@ -2,6 +2,7 @@
 """
 import pygame
 from pygame.locals import *  # pylint: disable=wildcard-import, unused-wildcard-import
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
 
 def assets_folder(filename):
@@ -27,3 +28,11 @@ def load_image(filename):
         string: the image object
     """
     return pygame.image.load(assets_folder(filename))
+
+
+def position_from_center(rect, pos_x, pos_y):
+    """Position a rect offset from center with pos x, pos y.
+
+    """
+    rect.x = ((SCREEN_WIDTH / 2) - (rect.width / 2)) + pos_x
+    rect.y = ((SCREEN_HEIGHT / 2) - (rect.height / 2)) + pos_y
